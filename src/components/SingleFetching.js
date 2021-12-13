876223b73e58890850c9ab42b796d6def0c08a4c
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-13 15:01:12 +0530

    feat button added

--- src/components/SingleFetching.js
+++ src/components/SingleFetching.js
@@ -4,18 +4,26 @@ import axios from "axios";
 function SingleDataFetching() {
   const [post, setPost] = useState({});
   const [id, setId] = useState(1);
+  const [idOnButtonClick, setIdOnButtonClick] = useState(1);
+
+  const getIdByButton = () => {
+    setIdOnButtonClick(id);
+  };
 
   useEffect(() => {
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .get(`https://jsonplaceholder.typicode.com/posts/${idOnButtonClick}`)
       .then((item) => setPost(item.data));
-  }, [id]);
+  }, [idOnButtonClick]);
   return (
     <div>
       <lable>
         Write the id here and you will see the title of the given id:
       </lable>
       <input value={id} onChange={(e) => setId(e.target.value)} />
+      <button type="button" onClick={getIdByButton}>
+        Fetch Title
+      </button>
       <h1> {post.title} </h1>
     </div>
   );
