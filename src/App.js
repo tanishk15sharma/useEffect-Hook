df3d294ef5ac917223b113aeba09a79ac996b400
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-13 14:02:24 +0530

    dataFetching-useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
-import InputCounter from "./components/CounteConditonally";
-import Counter from "./components/Counterone";
-import CountAuto from "./components/CountInterval";
+import DataFetching from "./components/DataFetching";
+// import InputCounter from "./components/CounteConditonally";
+// import Counter from "./components/Counterone";
+// import CountAuto from "./components/CountInterval";
 
 function App() {
   return (
     <div className="App">
       {/* <Counter /> */}
-      <CountAuto />
+      {/* <CountAuto /> */}
+      <DataFetching />
 
       {/* <InputCounter /> */}
     </div>
